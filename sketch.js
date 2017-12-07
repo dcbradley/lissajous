3a7dced2bacc282f62d955bb83a99c741a1ba7b5
Dan Bradley <dan@hep.wisc.edu>
2017-12-07 14:11:13 -0600

    Add a pause button.  Improve handling of time jumps and CPU overload.

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ var osc1,osc2,osc3;
 var start_t = 0;
 var w; // scale facter to map sound frequency to real time (slowing it down)
 var tstep;
+var t_off;
 var fx;
 var fy;
 var fz;
@@ -28,12 +29,16 @@ var scale_selector;
 var play_button;
 var mute_button;
 var mute_timer = null;
+var pause_button;
+var unpause_button;
 var speed_up_button;
 var slow_down_button;
 var speed_label;
+var speed_power;
 var bouncy_balls = true;
 var ratio_label;
 var reset_button;
+var max_frame_time;
 
 function equal_step(n) {
   return Math.pow(2,n/12.0);
@@ -129,12 +134,18 @@ function setup() {
   scale_selector.option("Well Tempered Scale","well");
   scale_selector.changed(newPlot);
 
-  play_button = createButton('Play');
+  play_button = createButton('Audio');
   play_button.mouseClicked(soundOn);
 
   mute_button = createButton('Mute');
   mute_button.mouseClicked(soundOff);
   mute_button.hide();
+
+  pause_button = createButton('Pause');
+  pause_button.mouseClicked(pauseDrawing);
+
+  unpause_button = createButton('Unpause');
+  unpause_button.mouseClicked(unpauseDrawing);
 
   speed_up_button = createButton('&#8593;');
   slow_down_button = createButton('&#8595;');
@@ -151,6 +162,9 @@ function setup() {
 
   noStroke();
   colorMode(HSB);
+
+  max_frame_time = 1.0/48;
+  frameRate(1.0/max_frame_time);
 
   osc1 = new p5.Oscillator();
   osc2 = new p5.Oscillator();
@@ -171,6 +185,8 @@ function draw() {
   var xoff = plot_x0+xamp;
   var yamp = (plot_y1-plot_y0)/2;
   var yoff = plot_y0+yamp;
+  var zamp = 360/2;
+  var zoff = 360/2;
   var bouncy_r = 4;
 
   var now = Date.now();
@@ -181,8 +197,10 @@ function draw() {
     start_t = now;
     last_t = 0;
     delta_t = 0;
-    last_x = -1;
-    bouncy_balls = w <= 0.01;
+    last_x = xoff; // always start at (0,0)
+    last_y = yoff;
+    last_z = zoff;
+    t_off = 0;
 
     fx_up_button.position(20,plot_y1+bouncy_r*2+4);
     fx_down_button.position(20,fx_up_button.position().y+fx_up_button.size().height);
@@ -203,52 +221,68 @@ function draw() {
 
     scale_selector.position(lpanel_x0,plot_y0 + (plot_y1-plot_y0)/2);
 
-    play_button.position(lpanel_x0,scale_selector.position().y+scale_selector.size().height+10);
-    mute_button.position(lpanel_x0,scale_selector.position().y+scale_selector.size().height+10);
-    reset_button.position(lpanel_x0,scale_selector.position().y+scale_selector.size().height+reset_button.size().height*2);
+    var button_height = reset_button.size().height;
+
+    play_button.position(lpanel_x0,scale_selector.position().y+scale_selector.size().height+button_height);
+    mute_button.position(lpanel_x0,scale_selector.position().y+scale_selector.size().height+button_height);
+    pause_button.position(lpanel_x0,scale_selector.position().y+scale_selector.size().height+button_height*2.5);
+    unpause_button.position(lpanel_x0,scale_selector.position().y+scale_selector.size().height+button_height*2.5);
+    reset_button.position(lpanel_x0,scale_selector.position().y+scale_selector.size().height+button_height*4);
 
     speed_up_button.position(plot_x0+(plot_x1-plot_x0)/2,fx_up_button.position().y);
     slow_down_button.position(plot_x0+(plot_x1-plot_x0)/2,speed_up_button.position().y+speed_up_button.size().height);
     speed_label.position(speed_up_button.position().x+speed_up_button.size().width+4,speed_up_button.position().y);
-    speed_label.html("Drawing speed: " + (w/(2*Math.PI)).toPrecision(2));
+    setSpeed();
 
     ratio_label.position(lpanel_x0,plot_y0+(plot_y1-plot_y0)/4);
     ratio_label.html("fx/fy: " + (fx/fy).toFixed(3) + "<br>fy/fx: " + (fy/fx).toFixed(3));
   }
 
   strokeWeight(4);
-
-  if( last_x >= 0 && bouncy_balls ) {
-    // erase bouncy balls on the x and y axes
-    stroke(0,0,0);
-    fill(0,0,0);
-    ellipse(last_x,plot_y1+bouncy_r*2,bouncy_r+1);
-    ellipse(plot_x1+bouncy_r*2,last_y,bouncy_r+1);
-  }
   noFill();
 
-  if( (delta_t-last_t)/tstep > 100 ) {
-    // just in case we can't keep up with real time, limit number of steps, to avoid long hangs
-    delta_t = last_t+tstep*100;
+  var last_bouncy_x = last_x;
+  var last_bouncy_y = last_y;
+
+  // If too much time passes between calls to draw(), that could indicate one of three things:
+  //   - the pause button was pressed
+  //   - execution was paused (e.g. computer was asleep or window was hidden)
+  //   - we aren't keeping up with frameRate()
+  // When that happens, skip some real time to avoid trying to catch up.
+  if( delta_t - last_t > max_frame_time ) {
+    t_off = t_off - (delta_t-max_frame_time-last_t);
+    last_t = delta_t-max_frame_time;
   }
-  for(var t=last_t+tstep; t<delta_t; t+=tstep) {
-    var x = Math.sin(fx*w*t)*xamp + xoff;
-    var y = Math.sin(fy*w*t)*yamp + yoff;
-    var z = Math.sin(fz*w*t)*360/2 + 360/2;
+
+  var next_t = last_t + tstep;
+  if( next_t > delta_t ) next_t = delta_t;
+  for(var t=next_t; t<=delta_t; t+=tstep) {
+    var x = Math.sin(fx*w*(t+t_off))*xamp + xoff;
+    var y = Math.sin(fy*w*(t+t_off))*yamp + yoff;
+    var z = Math.sin(fz*w*(t+t_off))*zamp + zoff;
 
     stroke(z,60,60);
-    if( last_x >= 0 ) {
-      line(last_x,last_y,x,y);
-    }
+    line(last_x,last_y,x,y);
 
     last_x = x;
     last_y = y;
     last_z = z;
     last_t = t;
   }
-  if( last_x >= 0 && bouncy_balls ) {
-    // draw bouncy balls along the x and y axis
-    stroke(last_z,60,60);
+
+  // draw bouncy balls along the x and y axis
+  if( bouncy_balls ) {
+    // erase previous position
+    stroke(0,0,0);
+    fill(0,0,0);
+    ellipse(last_bouncy_x,plot_y1+bouncy_r*2,bouncy_r+1);
+    ellipse(plot_x1+bouncy_r*2,last_bouncy_y,bouncy_r+1);
+  }
+  // turn off bouncy balls at high speed, because they get annoying
+  bouncy_balls = w <= 0.01;
+  if( bouncy_balls ) {
+    stroke(last_z,60,100);
+    fill(last_z,60,100);
     ellipse(last_x,plot_y1+bouncy_r*2,bouncy_r);
     ellipse(plot_x1+bouncy_r*2,last_y,bouncy_r);
   }
@@ -275,6 +309,8 @@ function newPlot() {
   osc1.freq(fx);
   osc2.freq(fy);
   osc3.freq(fz);
+
+  unpauseDrawing();
 }
 
 function windowResized() {
@@ -344,17 +380,27 @@ function soundOff() {
   }
 }
 
+function setSpeed() {
+  var phase = w*(last_t+t_off);
+  var speed = Math.pow(2,speed_power);
+  w = 2*Math.PI*speed;
+  t_off = phase/w - last_t; // adjust t_off so drawing position does not jump
+  tstep = 0.00001/speed;
+  if( speed < 1 ) {
+    speed_label.html("Animation speed: 1/" + Math.pow(2,-speed_power));
+  } else {
+    speed_label.html("Animation speed: " + speed);
+  }
+}
 function speedUp() {
-  if( w > 10 ) return;
-  w *= 2;
-  tstep /= 2;
-  newPlot();
+  if( speed_power >= 0 ) return;
+  speed_power += 1;
+  setSpeed();
 }
 function slowDown() {
-  if( w < 0.0001 ) return;
-  w /= 2;
-  tstep *= 2;
-  newPlot();
+  if( speed_power < -15 ) return;
+  speed_power -= 1;
+  setSpeed();
 }
 
 function startOver() {
@@ -363,8 +409,20 @@ function startOver() {
   fx_index = 4;
   fy_index = 7;
   fz_index = 0;
-  w = 0.005;
-  tstep = 0.01;
+  speed_power = -10;
+  setSpeed();
   soundOff();
+  unpauseDrawing();
   newPlot();
+}
+
+function pauseDrawing() {
+  pause_button.hide();
+  unpause_button.show();
+  noLoop();
+}
+function unpauseDrawing() {
+  pause_button.show();
+  unpause_button.hide();
+  loop();
 }
